5f2b68169c069a70d7c7c0a39889acdd89874553
JoaoGracindo <gabrielpessoa97@gmail.com.com>
2022-07-15 15:13:33 -0300

    html inside primary App components

--- projetos-modulo2/projeto7-instagramreact/src/components/App.js
+++ projetos-modulo2/projeto7-instagramreact/src/components/App.js
@@ -1,12 +1,16 @@
-
+import Corpo from "./Corpo";
+import FundoMobile from "./Fundo-mobile";
+import NavBar from "./NavBar";
 
 
 
 function App (){
     return (
-        <div>
-            
-        </div>
+        <>
+            <NavBar />
+            <Corpo />
+            <FundoMobile />
+        </>
     );
 }
 
