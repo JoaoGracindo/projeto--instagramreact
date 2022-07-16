4743ef6bfcb7b1f99f1b17dba2ee12160c680ecd
JoaoGracindo <gabrielpessoa97@gmail.com.com>
2022-07-16 01:38:49 -0300

    Componentização do SideBar

--- projetos-modulo2/projeto7-instagramreact/src/components/Corpo.js
+++ projetos-modulo2/projeto7-instagramreact/src/components/Corpo.js
@@ -1,25 +1,7 @@
 import Icon from "./icones";
 import Stories from "./Stories";
 import Posts from "./Posts";
-import Sugestoes from "./Sugestoes";
-
-function Sugestao (props){
-    const src = ('assets/img/' + props.user +'.svg');
-    return(
-      <div class="sugestao">
-        <div class="usuario">
-          <img src={src} />
-          <div class="texto">
-            <div class="nome">{props.user}</div>
-            <div class="razao">{props.razao}</div>
-          </div>
-        </div>
-
-        <div class="seguir">Seguir</div>
-      </div>
-    )
-}
-
+import SideBar from "./SideBar";
 
 function Corpo () {
     return(
@@ -32,39 +14,8 @@ function Corpo () {
 
         </div>
 
-        <div class="sidebar">
-          <div class="usuario">
-            <img src="assets/img/catanacomics.svg" />
-            <div class="texto">
-              <strong>catanacomics</strong>
-              Catana
-            </div>
-          </div>
+        <SideBar />
 
-          <Sugestoes />
-
-          {/* <div class="sugestoes">
-            <div class="titulo">
-              Sugestões para você
-              <div>Ver tudo</div>
-            </div>
-
-            <Sugestao user='bad.vibes.memes' razao='Segue você' />
-            <Sugestao user='chibirdart' razao='Segue você' />
-            <Sugestao user='razoesparaacreditar' razao='Novo no Instagram' />
-            <Sugestao user='adorable_animals' razao='Segue você' />
-            <Sugestao user='smallcutecats' razao='Segue você' />
-
-          </div> */}
-
-          <div class="links">
-            Sobre • Ajuda • Imprensa • API • Carreiras • Privacidade • Termos • Localizações • Contas mais relevantes • Hashtags • Idioma
-          </div>
-
-          <div class="copyright">
-            © 2021 INSTAGRAM DO FACEBOOK
-          </div>
-        </div>
       </div>
         
     )
